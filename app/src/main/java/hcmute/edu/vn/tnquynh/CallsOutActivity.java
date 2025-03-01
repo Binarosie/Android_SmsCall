70a15eb550ace102416a64407785f8d13732c986
Trần Như  Quỳnh <trankienquynh.070820@gmail.com>
2025-03-01 17:11:24 +0700

    add outgoing calls screen

---
+++ app/src/main/java/hcmute/edu/vn/tnquynh/CallsOutActivity.java
@@ -0,0 +1,13 @@
+package hcmute.edu.vn.tnquynh;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CallsOutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calls_out);
+    }
+}
